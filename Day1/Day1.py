3efd98025870e4e2034770400eb0e79e51f6dd1a
Joseph Zurita <JosephZurita@hotmail.com>
2022-12-03 23:41:32 +0000

    Day1

---
+++ Day1/Day1.py
@@ -0,0 +1,30 @@
+iElf   = 0
+lines  = []
+elfMax = []
+elfHi  = 0
+elfLo  = 0
+
+with open('Day1/input.txt') as file:
+    lines = file.readlines()
+
+for line in lines:
+    line = line.strip('\n')
+    if line == "":
+        if elfHi < elfLo:
+            elfHi = elfLo
+            elfMax.clear()
+            elfMax.append(iElf)
+        elif elfHi == elfLo:
+            elfMax.append(iElf)
+
+        iElf += 1
+        elfLo = 0
+
+        continue
+
+    if line.isdigit():
+        elfLo += int(line)
+
+print(elfMax)
+print(elfHi)
+    
