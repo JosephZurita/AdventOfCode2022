0a1c31c78d746187cdd519f1e6daa2609d793f84
Joseph Zurita <JosephZurita@hotmail.com>
2022-12-06 13:23:30 +0000

    Day 4 Part 2, actually easier lmao

---
+++ Day4/Part2.py
@@ -0,0 +1,16 @@
+lines    = []
+pairList = []
+iSum     = 0
+
+with open('Day4/input.txt') as file:
+    lines = file.readlines()
+
+for line in lines:
+    line = line.strip('\n')
+    pairList = [p.split("-") for p in line.split(",")]
+    pairList = [[int(item[0]), int(item[1])] for item in pairList]
+    if pairList[0][0] <= pairList[1][1] and pairList[0][1] >= pairList[1][0]:
+        iSum += 1
+
+    
+print(iSum)
