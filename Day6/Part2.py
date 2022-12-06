c9e2b3952b0a3830f38289642c8791766c3a04ff
Joseph Zurita <JosephZurita@hotmail.com>
2022-12-06 18:29:31 +0000

    Day 6 Part 2, Basically just a parameterised version

--- Day6/Part2.py
+++ Day6/Part2.py
@@ -1,19 +1,19 @@
 dataStream   = []
 packetMarker = []
 iChar        = 0
+iDistinct    = 14
 
 
 with open('Day6/input.txt') as file:
     lines = file.readlines()
 
 dataStream = list(lines[0])
-packetMarker = [dataStream[i] for i in range(4)]
+packetMarker = [dataStream[i] for i in range(iDistinct)]
 
-for iChar in range(4, len(dataStream)):
-    if len(set(packetMarker)) == 4:
+for iChar in range(iDistinct, len(dataStream)):
+    if len(set(packetMarker)) == iDistinct:
         break
     packetMarker.pop(0)
     packetMarker.append(dataStream[iChar])
-    
 print(packetMarker)
 print(iChar)
